d7716356c9346e28f605973bb544974f7e75ad78
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-06-23 16:47:04 -0300

    test - testing granTotal context value changes

--- sundaes-on-demand/src/pages/entry/test/totalUpdates.spec.tsx
+++ sundaes-on-demand/src/pages/entry/test/totalUpdates.spec.tsx
@@ -1,6 +1,8 @@
 import { render, screen, waitFor } from "../../../utils/testing-library-utils";
 import userEvent from "@testing-library/user-event";
 import Options from "../Options";
+import OrderEntry from "../OrderEntry";
+import { PRICE_PER_ITEM } from "../../../utils/constants";
 
 describe("Testing totalUpdates", () => {
   it("should update scoop subtotal when scoops changes", async () => {
@@ -36,6 +38,150 @@ describe("Testing totalUpdates", () => {
 
     await waitFor(async () => {
       expect(scoopsSubTotal).toHaveTextContent("6.00");
+    });
+  });
+
+  it("should grand total starts at $0.00", () => {
+    render(<OrderEntry />);
+
+    const grandTotal = screen.getByRole("heading", {
+      name: /grand total: \$/i,
+    });
+
+    expect(grandTotal).toHaveTextContent("0.00");
+  });
+
+  it("should grand total updates properly if scoop is added first", async () => {
+    render(<OrderEntry />);
+    const grandTotal = screen.getByRole("heading", {
+      name: /grand total: \$/i,
+    });
+
+    const scoopsQuantity = 2;
+
+    const vanillaInput = await screen.findByRole("spinbutton", {
+      name: "Vanilla",
+    });
+
+    expect(vanillaInput).toBeInTheDocument();
+
+    userEvent.clear(vanillaInput);
+    userEvent.type(vanillaInput, `${scoopsQuantity}`);
+
+    await waitFor(async () => {
+      expect(grandTotal).toHaveTextContent(
+        `${PRICE_PER_ITEM["scoops"] * scoopsQuantity}`
+      );
+    });
+
+    const cherryCheckbox = await screen.findByRole("checkbox", {
+      name: "Cherries",
+    });
+
+    expect(cherryCheckbox).toBeInTheDocument();
+
+    userEvent.click(cherryCheckbox);
+
+    await waitFor(async () => {
+      expect(grandTotal).toHaveTextContent(
+        `${
+          PRICE_PER_ITEM["scoops"] * scoopsQuantity + PRICE_PER_ITEM["toppings"]
+        }`
+      );
+    });
+  });
+
+  it("should grand total updates properly if scoop is added first", async () => {
+    render(<OrderEntry />);
+    const grandTotal = screen.getByRole("heading", {
+      name: /grand total: \$/i,
+    });
+
+    const scoopsQuantity = 2;
+
+    const vanillaInput = await screen.findByRole("spinbutton", {
+      name: "Vanilla",
+    });
+
+    expect(vanillaInput).toBeInTheDocument();
+
+    userEvent.clear(vanillaInput);
+    userEvent.type(vanillaInput, `${scoopsQuantity}`);
+
+    await waitFor(async () => {
+      expect(grandTotal).toHaveTextContent(
+        `${PRICE_PER_ITEM["scoops"] * scoopsQuantity}`
+      );
+    });
+
+    const cherryCheckbox = await screen.findByRole("checkbox", {
+      name: "Cherries",
+    });
+
+    expect(cherryCheckbox).toBeInTheDocument();
+
+    userEvent.click(cherryCheckbox);
+
+    await waitFor(async () => {
+      expect(grandTotal).toHaveTextContent(
+        `${
+          PRICE_PER_ITEM["scoops"] * scoopsQuantity + PRICE_PER_ITEM["toppings"]
+        }`
+      );
+    });
+  });
+
+  it("should grand total updates properly after removing items", async () => {
+    render(<OrderEntry />);
+
+    const grandTotal = screen.getByRole("heading", {
+      name: /grand total: \$/i,
+    });
+
+    const cherriesCheckbox = await screen.findByRole("checkbox", {
+      name: "Cherries",
+    });
+
+    expect(cherriesCheckbox).toBeInTheDocument();
+
+    userEvent.click(cherriesCheckbox);
+
+    let scoopsQuantity = 2;
+
+    const vanillaInput = await screen.findByRole("spinbutton", {
+      name: "Vanilla",
+    });
+
+    expect(vanillaInput).toBeInTheDocument();
+
+    userEvent.clear(vanillaInput);
+    userEvent.type(vanillaInput, `${scoopsQuantity}`);
+
+    await waitFor(async () => {
+      expect(grandTotal).toHaveTextContent(
+        `${
+          PRICE_PER_ITEM["scoops"] * scoopsQuantity + PRICE_PER_ITEM["toppings"]
+        }`
+      );
+    });
+
+    userEvent.click(cherriesCheckbox);
+
+    await waitFor(async () => {
+      expect(grandTotal).toHaveTextContent(
+        `${PRICE_PER_ITEM["scoops"] * scoopsQuantity}`
+      );
+    });
+
+    scoopsQuantity = 1;
+
+    userEvent.clear(vanillaInput);
+    userEvent.type(vanillaInput, `${scoopsQuantity}`);
+
+    await waitFor(async () => {
+      expect(grandTotal).toHaveTextContent(
+        `${PRICE_PER_ITEM["scoops"] * scoopsQuantity}`
+      );
     });
   });
 });
