38bbf5f00a363241e49ce7e003738dd12670174c
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-06-23 16:45:59 -0300

    feat - add formatCurrency

--- sundaes-on-demand/src/pages/entry/OrderEntry.tsx
+++ sundaes-on-demand/src/pages/entry/OrderEntry.tsx
@@ -1,10 +1,17 @@
+import { useOrderDetailsContext } from "../../contexts/OrderDetailsContext";
+import { formatCurrency } from "../../utils/functions";
 import Options from "./Options";
 
 const OrderEntry = () => {
+  const [OrderDetailsContext, updateItemCount] = useOrderDetailsContext();
+
   return (
     <>
       <Options optionType='scoops' />
       <Options optionType='toppings' />
+      <h2>
+        Grand Total: {formatCurrency(OrderDetailsContext.totals.grandTotal)}
+      </h2>
     </>
   );
 };
