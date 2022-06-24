6c684a47c5fe4baf9f116bd934cf947168d2bd75
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-06-24 17:12:59 -0300

    feat - add OrderConfirmation component

---
+++ sundaes-on-demand/src/pages/confirmation/OrderConfirmation.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
+import { useOrderDetailsContext } from "../../contexts/OrderDetailsContext";
+type OrderPhases = "INPROGRESS" | "REVIEW" | "COMPLETED";
+
+interface OrderConfirmationProps {
+  setOrderPhase: (orderPhase: OrderPhases) => void;
+}
+
+const OrderConfirmation = ({ setOrderPhase }: OrderConfirmationProps) => {
+  const [, , resetOrder] = useOrderDetailsContext();
+
+  const [orderNumber, setOrderNumber] = useState<number | null>(null);
+  useEffect(() => {
+    axios
+      .post("http://localhost:3030/order")
+      .then((response) => {
+        setOrderNumber(response.data.orderNumber);
+      })
+      .catch((error) => {
+        //TODO: handle error
+      });
+  }, []);
+
+  function handleClick() {
+    resetOrder();
+    setOrderPhase("INPROGRESS");
+  }
+
+  if (orderNumber) {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h1>Thank You!</h1>
+        <p>Your order number is {orderNumber}</p>
+        <p style={{ fontSize: "25%" }}>
+          {" "}
+          as per our terms and conditions, nothing wil happen now
+        </p>
+        <Button onClick={handleClick}>Create new order</Button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Loading!!!</h1>
+      </div>
+    );
+  }
+};
+
+export default OrderConfirmation;
