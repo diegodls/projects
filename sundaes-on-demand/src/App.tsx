bc823087098945d9b381aa0aa7c045d1c054d119
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-06-24 17:12:01 -0300

    feat - add order phases

--- sundaes-on-demand/src/App.tsx
+++ sundaes-on-demand/src/App.tsx
@@ -1,14 +1,39 @@
+import { useState } from "react";
 import { Container } from "react-bootstrap";
 import { OrderDetailsProvider } from "./contexts/OrderDetailsContext";
+import OrderConfirmation from "./pages/confirmation/OrderConfirmation";
 import OrderEntry from "./pages/entry/OrderEntry";
+import OrderSummary from "./pages/summary/OrderSummary";
+
+type OrderPhases = "INPROGRESS" | "REVIEW" | "COMPLETED";
+
 function App() {
+  const [orderPhase, setOrderPhase] = useState<OrderPhases | null>(
+    "INPROGRESS"
+  );
+
+  let Component = <OrderEntry setOrderPhase={setOrderPhase} />;
+
+  switch (orderPhase) {
+    case "INPROGRESS":
+      Component = <OrderEntry setOrderPhase={setOrderPhase} />;
+      break;
+    case "REVIEW":
+      Component = <OrderSummary setOrderPhase={setOrderPhase} />;
+      break;
+    case "COMPLETED":
+      Component = <OrderConfirmation setOrderPhase={setOrderPhase} />;
+      break;
+    default:
+  }
+
   return (
     <Container>
       <OrderDetailsProvider>
         {/* Summary page and entry page need provider */}
-        <OrderEntry />
-        {/* Confirmation page does not nee provider */}
+        <Container>{Component}</Container>
       </OrderDetailsProvider>
+      {/* Confirmation page does not need provider */}
     </Container>
   );
 }
