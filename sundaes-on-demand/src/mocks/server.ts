66b2e67d5227978f27884c4f1c88ea43161b90d8
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-07-03 10:42:05 -0300

    Creating assets

--- sundaes-on-demand/src/mocks/server.ts
+++
@@ -1,4 +0,0 @@
-import { setupServer } from "msw/node";
-import { handlers } from "./handles";
-
-export const server = setupServer(...handlers);
--- sundaes-on-demand/src/utils/functions.ts
+++
@@ -1,9 +0,0 @@
-function formatCurrency(amount: number) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format(amount);
-}
-
-export { formatCurrency };
